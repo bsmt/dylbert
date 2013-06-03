a989f14ba036f23ad2066857083c7bc6b361f2f9
bsmt <bsmt@krax.in>
2013-06-03 11:59:15 -0600

    Setup project and submodules

---
+++ dylbert/dylbert/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  dylbert
+//
+//  Created by bsmt on 6/3/13.
+//  Copyright (c) 2013 dil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
