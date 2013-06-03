bdcc3923661c4859ef758c9d7a3cea2a587baba4
bsmt <bsmt@krax.in>
2013-06-03 12:41:11 -0600

    Setup CLI bullshit

--- dylbert/dylbert/main.m
+++ dylbert/dylbert/main.m
@@ -7,16 +7,64 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "GBCommandLineParser.h"
+#import "GBOptionsHelper.h"
+#import "GBSettings.h"
 
-int main(int argc, const char * argv[])
+int main(int argc, char **argv)
 {
+@autoreleasepool
+{
+    GBSettings *settings = [GBSettings settingsWithName:@"Settings" parent:nil];
+    GBOptionsHelper *options = [[GBOptionsHelper alloc] init];
+    options.applicationName = ^{return @"dylbert";};
+    
+    NSString *targetDesc = @"The .app whose binary you want to add a dylib to.";
+    [options registerOption:'t' long:@"target" description:targetDesc
+                      flags:GBValueRequired];
+    
+    NSString *dylibDesc = @"The dylib that will be added to the app.";
+    [options registerOption:'d' long:@"dylib" description:dylibDesc
+                      flags:GBValueRequired];
+    [options registerOption:'h' long:@"help" description:@"Display help."
+                      flags:GBValueNone];
+    __block BOOL printHelp = FALSE;
+    
+    GBCommandLineParser *parser = [[GBCommandLineParser alloc] init];
+    [options registerOptionsToCommandLineParser:parser];
+    
+    [parser parseOptionsWithArguments:argv count:argc block:^(GBParseFlags flags,
+                                                              NSString *option,
+                                                              id value, BOOL *stop)
+    {
+        switch (flags)
+        {
+            case GBParseFlagUnknownOption:
+                printf("Unknown option: %s, see --help\n", option.UTF8String);
+                break;
+            case GBParseFlagMissingValue:
+                printf("Missing value for %s\n", option.UTF8String);
+            case GBParseFlagArgument:
+                [settings addArgument:value];
+                break;
+            case GBParseFlagOption:
+                if ([option isEqualToString:@"help"] || [option isEqualToString:@"h"]
+                    || [option isEqualToString:@"?"])
+                {
+                    printHelp = TRUE;
+                }
 
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+                [settings addArgument:value];
+                break;
+        }
+    }];
+    
+    if (printHelp == TRUE)
+    {
+        [options printHelp];
     }
+}
+    
     return 0;
 }
 
